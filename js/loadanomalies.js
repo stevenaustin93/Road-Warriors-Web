86dff99e885e3900858e1eefdd5acbec1848c54e
mvs5465 <schwartz_matthew2@bah.com>
2015-06-18 08:33:18 -0400

    Load Anomalies Checkboxes Functioning
    Added jquery script to make checkboxes function properly
    changed list of event checkboxes
    added functionality to "clear map" button

--- js/loadanomalies.js
+++ js/loadanomalies.js
@@ -1,7 +1,6 @@
 //
 // loadanomalies.js
-// This file handles the checkbox selection display of anomalies on the map
-// (.csv file is loaded into a JS array)
+// This (revamped) script handles the checkbox input of anomalies on the map
 //
 
 // Icon paths:
@@ -12,30 +11,141 @@ var BREAKING_ICON = BASE_PATH + "\\images\\breaking_icon.png";
 var FAST_ACCEL_ICON = BASE_PATH + "\\images\\fast_accel_icon.png";
 var GENERAL_ALERT_ICON = BASE_PATH + "\\images\\general_alert.png";
 
-/*
-//
-// Function to read file data from a server-side file using jquery
-function readFile(inputFile) {
-    
-    // Input variable
-	var input;
 
-	// Load content of file into local input var
-	jQuery.get(ANOMALIES_FILE, function(data) {
-		alert(data);
-		input = data;
+$(document).ready(function() {
+	// The DOM is ready
+	alert("DOM ready!");
+
+	// Load .csv data into array
+	var anomaliesArray = populateArray();
+
+	// Create and populate array for each type of marker
+	var swerveArray = new Array();
+	var rapidDecelArray = new Array();
+	var rapidAccelArray = new Array();
+	var generalArray = new Array();
+
+	// Loop through anomaliesArray and create a marker in respective arrays
+	for (var i = 0; i < anomaliesArray.length; i++) {
+
+		// GPS Position
+		var markPos = new google.maps.LatLng(anomaliesArray[i][0], anomaliesArray[i][1]);
+
+		// Set icon and title (label) accordingly
+		switch (anomaliesArray[i][2]) {
+			case "b":
+				markIcon = BREAKING_ICON;
+				markTitle = "Rapid breaking event detected here!";
+				break;
+			case "y":
+				markIcon = SWERVE_ICON;
+				markTitle = "Swerving event detected here!";
+				break;
+			case "a":
+				markIcon = FAST_ACCEL_ICON;
+				markTitle = "Fast acceleration event detected here!";
+				break;
+			default:
+				markIcon = GENERAL_ALERT_ICON;
+				markTitle = "General anomaly detected here!";
+		}
+
+		// Create the marker
+		var marker = new google.maps.Marker({
+	    	position: 	markPos,
+	    	title: 		markTitle,
+	    	icon: 		markIcon
+	  	});
+
+		// Add the marker to respective array
+	  	switch (anomaliesArray[i][2]) {
+	  		case "b":
+				rapidDecelArray.push(marker);
+				break;
+			case "y":
+				swerveArray.push(marker);
+				break;
+			case "a":
+				rapidAccelArray.push(marker);
+				break;
+			default:
+				generalArray.push(marker);
+	  	}
+	}
+
+	alert("Marker arrays successfuly populated!");
+
+	// Swerve checkbox event handler
+	$('#swerve').click(function() {
+		for (var i = 0; i < swerveArray.length; i++) {
+			if(document.getElementById('swerve').checked){
+				swerveArray[i].setMap(map);
+			}
+			else {
+				swerveArray[i].setMap(null);
+			}
+		}
 	});
 
-	// Return input
-    return (input);
+	// Rapid decel checkbox event handler
+	$('#rapidDecel').click(function() {
+		for (var i = 0; i < rapidDecelArray.length; i++) {
+			if(document.getElementById('rapidDecel').checked){
+				rapidDecelArray[i].setMap(map);
+			}
+			else {
+				rapidDecelArray[i].setMap(null);
+			}
+		}
+	});
 
-}
-*/
+	// Rapid accel checkbox event handler
+	$('#rapidAccel').click(function() {
+		for (var i = 0; i < rapidAccelArray.length; i++) {
+			if(document.getElementById('rapidAccel').checked){
+				rapidAccelArray[i].setMap(map);
+			}
+			else {
+				rapidAccelArray[i].setMap(null);
+			}
+		}
+	});
 
-//alert( jQuery.ready() );
+	// General anomaly checkbox event handler
+	$('#general').click(function() {
+		for (var i = 0; i < generalArray.length; i++) {
+			if(document.getElementById('general').checked){
+				generalArray[i].setMap(map);
+			}
+			else {
+				generalArray[i].setMap(null);
+			}
+		}
+	});
 
-//
-// Function to convert .csv input to a useable 2D array
+	// Clear map event handler
+	$('#clearMap').click( function() {
+		for (var i = 0; i < swerveArray.length; i++) {
+			swerveArray[i].setMap(null);
+		}
+		for (var i = 0; i < rapidDecelArray.length; i++) {
+			rapidDecelArray[i].setMap(null);
+		}
+		for (var i = 0; i < rapidAccelArray.length; i++) {
+			rapidAccelArray[i].setMap(null);
+		}
+		for (var i = 0; i < generalArray.length; i++) {
+			generalArray[i].setMap(null);
+		}
+
+		$('.chckbx').each(function() {
+			this.checked = false;
+		});
+
+	})
+
+});
+
 function populateArray() {
 
 	// Variable Declarations
@@ -51,122 +161,4 @@ function populateArray() {
 
 	// Return the array of information
 	return(anomaliesArr);
-}
-
-//
-// Testing?
-$(document).on("click", "rapidDecel", function() {
-	placeMarkers();
-});
-//
-// Function to place markers on map
-function placeMarkers() {
-
-	//////
-	// The follow are optimization steps, to be taken later:
-	// 	Check to see if the marker data has been loaded
-	// 	Load marker data if needed
-	//	Check to see if the marker arrays exist
-	//////
-
-
-	// Variable Declarations
-	var anomaliesArray;
-
-	var standStillMarkers = new Array();
-	var crashMarkers = new Array();
-	var swerveMarkers = new Array();
-	var rapidAccelMarkers = new Array();
-	var rapidDecelMarkers = new Array();
-	var generalMarkers = new Array();
-
-	// Get .csv data into 2D array
-	anomaliesArray = populateArray();
-
-	// Loop through array and create markers
-	for (var i = 0; i < anomaliesArray.length; i++) {
-		var marker;
-		var markPos;
-		var markIcon;
-		var markTitle;
-
-		markPos = new google.maps.LatLng(anomaliesArray[i][0], anomaliesArray[i][1]);
-
-		switch (anomaliesArray[i][2]) {
-			case "b":
-				markIcon = BREAKING_ICON;
-				markTitle = "Rapid breaking event detected here!";
-				break;
-			case "y":
-				markIcon = SWERVE_ICON;
-				markTitle = "Swerving event detected here!";
-				break;
-			case "a":
-				markIcon = FAST_ACCEL_ICON;
-				markTitle = "Fast acceleration event detected here!";
-				break;
-			default:
-				markTitle = "General anomaly detected here!";
-				markIcon = GENERAL_ALERT_ICON;
-		}
-
-		marker = new google.maps.Marker({
-	    	position: 	markPos,
-	    	title: 		markTitle,
-	    	icon: 		markIcon
-	  	});
-
-	  	switch (anomaliesArray[i][2]) {
-	  		case "b":
-				rapidDecelMarkers.push(marker);
-				break;
-			case "y":
-				swerveMarkers.push(marker);
-				break;
-			case "a":
-				rapidAccelMarkers.push(marker);
-				break;
-			default:
-				generalMarkers.push(marker);
-
-	  	}
-	}
-
-	if ( document.getElementById('rapidDecel') == null) {
-		alert("Uh oh, the element 'rapidDecel' doesn't exist!");
-	}
-  	if ( document.getElementById('rapidDecel').checked ) {
-  		for (var i = 0; i < rapidDecelMarkers.length; i++) {
-  			rapidDecelMarkers[i].setMap(map);
-  		}
-  	} else {
-  		for (var i = 0; i < rapidDecelMarkers.length; i++) {
-  			rapidDecelMarkers[i].setMap(null);
-  		}
-  	}
-
-  	if ( document.getElementById('swerve').checked ) {
-  		for (var i = 0; i < swerveMarkers.length; i++) {
-  			swerveMarkers[i].setMap(map);
-  		}
-  	} else {
-  		for (var i = 0; i < swerveMarkers.length; i++) {
-  			swerveMarkers[i].setMap(null);
-  		}
-  	}
-
-  	if ( document.getElementById('rapidAccel').checked ) {
-  		for (var i = 0; i < rapidAccelMarkers.length; i++) {
-  			rapidAccelMarkers[i].setMap(map);
-  		}
-  	} else {
-  		for (var i = 0; i < rapidAccelMarkers.length; i++) {
-  			rapidAccelMarkers[i].setMap(null);
-  		}
-  	}
-	
-}
-
-function clearMarkers() {
-	map.clearOverlays();
 }
