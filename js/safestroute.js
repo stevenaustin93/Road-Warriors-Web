38e4b01d8d4f87b1adc851c58200ae2a0a009c21
mvs5465 <schwartz_matthew2@bah.com>
2015-06-19 09:42:30 -0400

    safestroutejs
    added nothing

---
+++ js/safestroute.js
@@ -0,0 +1,14 @@
+//
+// safestroute.js
+// This is the heavyweight file, this takes in a route (or creates a route) and outputs a matrix of GPS coordinates along the route.
+// Assume: 
+//	- A route currently exists on the map
+//	- It is the only route on the map
+//	- The route contains at least several data points of which we have data
+//
+
+$(document).ready(function() {
+	// Basic Functionality:
+	// A route is created or exists on the map
+	// Create a list of 
+})
