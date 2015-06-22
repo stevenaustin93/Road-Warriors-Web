fa60e645c3db640e0d725d87b34e87b0a31e12b2
mvs5465 <schwartz_matthew2@bah.com>
2015-06-22 10:52:43 -0400

    reduced imports to one file
    removed many <src> calls into one <src main> include

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//
+// main.js
+// This file handles the importing of all relevant scripts
+//
+
+$.getScript("js/loadanomalies.js");
+$.getScript("js/jquery-csv.js");
+$.getScript("js/colorroute.js");
+$.getScript("js/brakemap.js");
+$.getScript("js/heatmap.js");
