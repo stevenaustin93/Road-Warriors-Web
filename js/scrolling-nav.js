548482c9d6bbc443e30bf617b6d933ef1308455e
nrahmani <nrahmani110@gmail.com>
2015-07-01 09:57:49 -0400

    beginning of adding bootstrap

---
+++ js/scrolling-nav.js
@@ -0,0 +1,12 @@
+//jQuery to collapse the navbar on scroll
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
