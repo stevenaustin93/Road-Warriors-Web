4ef3ce8aa79aa5e45e514557647e4b1a3d699199
mvs5465 <schwartz_matthew2@bah.com>
2015-07-07 15:27:45 -0400

    Consolidated heatmaps

--- js/heatmap.js
+++ js/heatmap.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
 	});
 });
 
+/*
 function HeatmapType() {
 	// Get the dropdown menu value
 	var heatmapVal = $('#heatMap').val();
@@ -67,12 +68,17 @@ function HeatmapType() {
 	return (returnValue);
 
 }
+*/
 
 //
 // Query the general table on parse for the specified column
 function QueryForHeatmap() {
 	// Determine which type of heatmap we want
-	var heatmapCol = HeatmapType();
+	var heatmapCol = $('#heatMap').val();
+
+	// Setup query from general table
+	var rowColPairs = Parse.Object.extend("generalTable");
+	var query = new Parse.Query(rowColPairs);
 
 	if (heatmapCol === "none") {
 
@@ -86,14 +92,35 @@ function QueryForHeatmap() {
 		QueryForCarHeatmap();
 		return;
 
+	} else if (heatmapCol === "accel") {
+
+		var accel1Query = new Parse.Query(rowColPairs);
+		var accel2Query = new Parse.Query(rowColPairs);
+
+		accel1Query.greaterThan("accel1", 0);
+		accel2Query.greaterThan("accel2", 0);
+
+		query = Parse.Query.or(accel1Query, accel2Query);
+
+	} else if (heatmapCol === "brake") {
+
+		var brake1Query = new Parse.Query(rowColPairs);
+		var brake2Query = new Parse.Query(rowColPairs);
+
+		brake1Query.greaterThan("brake1", 0);
+		brake2Query.greaterThan("brake2", 0);
+
+		query = Parse.Query.or(brake1Query, brake2Query);
+
+	} else if (heatmapCol === "variance") {
+
+		QueryForVarHeatmap();
+		return;
+
+	} else {
+		return;
 	}
 
-	// Setup query from general table
-	var rowColPairs = Parse.Object.extend("generalTable");
-	var query = new Parse.Query(rowColPairs);
-
-	// Set query constraints
-	query.greaterThan(heatmapCol, 0);
 	query.limit(500);
 
 	// Perform query
@@ -110,8 +137,8 @@ function QueryForHeatmap() {
 				var object = results[i];
 
 				// Convert row/col to lat/lng
-				var GLat = object.get('row') * 0.00005 + 41.994;
-				var GLng = object.get('col') * 0.00005 - 84.88087;
+				var GLat = object.get('row') * 0.00005 + 35;
+				var GLng = object.get('col') * 0.00005 - 90;
 
 				// Add point to coords array
 				googleLatLngArray.push(new google.maps.LatLng(GLat, GLng));
@@ -132,23 +159,23 @@ function QueryForHeatmap() {
 
 
 //
-// Function to query for only top 5% of car densities
-function QueryForCarHeatmap() {
+// Function to query for only top of speed variances
+function QueryForVarHeatmap() {
 
 	// Setup query from general table
 	var rowColPairs = Parse.Object.extend("generalTable");
-	var carQuery = new Parse.Query(rowColPairs);
+	var varQuery = new Parse.Query(rowColPairs);
 
 	// Set query constraints
-	carQuery.descending("cars");
-	carQuery.limit(1000);
+	varQuery.descending("speed_variance");
+	varQuery.limit(1000);
 
 	// Perform query
-	carQuery.find({
+	varQuery.find({
 
 		success: function(results) {
 
-			//alert("Number of car heatmap entries: " + results.length);
+			alert("Number of car heatmap entries: " + results.length);
 
 			// Create array of lat/longs for the heatmap plot
 			var googleLatLngArray = new Array();
@@ -157,8 +184,8 @@ function QueryForCarHeatmap() {
 				var object = results[i];
 
 				// Convert row/col to lat/lng
-				var GLat = object.get('row') * 0.00005 + 41.994;
-				var GLng = object.get('col') * 0.00005 - 84.88087;
+				var GLat = object.get('row') * 0.00005 + 35;
+				var GLng = object.get('col') * 0.00005 - 90;
 
 				// Add point to coords array
 				googleLatLngArray.push(new google.maps.LatLng(GLat, GLng));
@@ -177,6 +204,51 @@ function QueryForCarHeatmap() {
 
 }
 
+//
+// Function to query for only top car densities
+function QueryForCarHeatmap() {
+
+	// Setup query from general table
+	var rowColPairs = Parse.Object.extend("generalTable");
+	var carQuery = new Parse.Query(rowColPairs);
+
+	// Set query constraints
+	carQuery.descending("cars");
+	carQuery.limit(1000);
+
+	// Perform query
+	carQuery.find({
+
+		success: function(results) {
+
+			alert("Number of car heatmap entries: " + results.length);
+
+			// Create array of lat/longs for the heatmap plot
+			var googleLatLngArray = new Array();
+
+			for (var i = 0; i < results.length; i++) {
+				var object = results[i];
+
+				// Convert row/col to lat/lng
+				var GLat = object.get('row') * 0.00005 + 35;
+				var GLng = object.get('col') * 0.00005 - 90;
+
+				// Add point to coords array
+				googleLatLngArray.push(new google.maps.LatLng(GLat, GLng));
+			}
+
+			// Send coords array to plotting function
+			PlotHeatmap(googleLatLngArray);
+
+		},
+
+		error: function(error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
+
+	});
+
+}
 
 //
 // Function that takes array of google.maps.LatLng() values and plots a heatmap
