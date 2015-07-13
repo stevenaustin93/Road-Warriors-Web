1d4f72b97610a59e30fd8ee000dfd42a67721108
landraka <landraka@vt.edu>
2015-07-13 15:09:22 -0400

    pretty fly for a shlong
    all "works" need to implement it

--- js/clickDrag.js
+++ js/clickDrag.js
@@ -1,58 +1,47 @@
 //click and drag
 $(document).ready(function() {
 	var clickDragButtonCLICKED = false;
+	var queryClicked = false;
 	$('#clkDrg').click(function(){ //assuming my buttons name is #clkDrg
 		clickDragFUNC(clickDragButtonCLICKED);
 		if (clickDragButtonCLICKED) this.blur();
 		clickDragButtonCLICKED = !clickDragButtonCLICKED;
 	})
 
+	$('#query').click(function(){
+		queryClicked = true;
+		if (queryClicked) this.blur();
+		queryClicked = !queryClicked;
+	})
+
 }); 
 
+
 function clickDragFUNC(isClicked){
-	//locks the viewport and scaling
-	options = $.extend({
-	    scrollwheel: false,
-	    navigationControl: false,
-	    mapTypeControl: false,
-	    scaleControl: false,
-	    draggable: false,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	}, options);
 
-	//detects a drag, when mouse clicks down it sets the place and on mouse click up it check if it moved
-	//if it did move then it will execute a function
-	var isDragging = false;
-	var lat1, longi1, lat2, longi2 = 0;
-	$('#map_canvas') 
-    .mousedown(function() {
-    	//query google maps for location of mouse, save it
-        isDragging = false;
-        //here save first coordinate pair
-    })
-    .mousemove(function() {
-    	//query google maps for new location of mouse and save
-        isDragging = true;
-        //here save second coordinate pair
-     })
-    .mouseup(function() {
-        var wasDragging = isDragging;
-        isDragging = false;
-        if (wasDragging) { //if it was dragging...
-            //querry server for square of data between 2 coordinate pairs
-        }
-        //unclick button
-
-        //unlock map
-        options = $.extend({
-	    	scrollwheel: true,
-	   		navigationControl: true,
-	    	mapTypeControl: true,
-	    	scaleControl: true,
-	    	draggable: true,
-	    	mapTypeId: google.maps.MapTypeId.ROADMAP
-		}, options);
-
+	var bounds = new google.maps.LatLngBounds(  ///needs to be changed so it auto sets itself in the users viewport
+      new google.maps.LatLng(51.41859298, 0.089179345),
+      new google.maps.LatLng(51.45, 0.25)
+  	);
+  	var gotBounds = new google.maps.LatLngBounds();
+	var lat1, long1, lat2, long2 = 0;
+	var rectangle = new google.maps.Rectangle({
+    	bounds: bounds,
+    	strokeColor: '#FF0000',
+    	strokeOpacity: 0.8,
+    	strokeWeight: 2,
+    	fillColor: '#FF0000',
+    	fillOpacity: 0.35,
+    	draggable: true,
+    	editable: true
+ 	 });
+    rectangle.setMap(map);
+    google.maps.event.addListener(rectangle, 'bounds_changed',function(event){
+       gotBounds = rectangle.getBounds();
+       if(queryClicked==true){
+       	//querry
+       	queryClicked=false;
+       }
     });
 
 }
