f62c5ac13c9a02a63c593d824c40887d3353a415
mvs5465 <schwartz_matthew2@bah.com>
2015-07-16 12:24:06 -0400

    Changed summary text in drag select box

--- js/clickDrag.js
+++ js/clickDrag.js
@@ -233,8 +233,8 @@ function makeRectInfo(results) {
 	message += "<div><b>Bounds: </b> (" + ne.lat().toPrecision(4) + "," + ne.lng().toPrecision(4) + ") to (" +
 		sw.lat().toPrecision(4) + "," + sw.lng().toPrecision(4) + ")";
 	message += "<div><b>Average Safety Rating: </b> " + avgsafety.toPrecision(2) +"/10";
-	message += "<div style='max-width: 200px'><b>Summary: </b>Connected Vehicles have reported " + numincidents +
-	 " incidents in this area, including rapid acceleration, hard braking, or swerving.";
+	message += "<div style='max-width: 200px'><b>Summary: </b>Connected Vehicle data indicates that " + numincidents +
+	 " driving irregularities have occured in this area, including rapid acceleration, hard braking, or swerving.";
 	/*
 	message += "<div><b>Rapid Accelerations: </b> " + numaccels;
 	message += "<div><b>Driver Swerves: </b> " + numswerves;
