f3ef60150aa6fb5ae11de3c2b24efdcb82a81361
mvs5465 <schwartz_matthew2@bah.com>
2015-07-16 08:40:39 -0400

    Added tooltips, moved map initialization scripts to mapinit.js and cleaned index.html

--- js/mapinit.js
+++ js/mapinit.js
@@ -1,35 +1,83 @@
-//
-// mapinit.js
-// This file initializes the Google Map element of the page
-//
-
 var map;
-var mapCanvas;
-var homePos;
-var mapOptions;
 
 function initialize() {
-	mapCanvas = document.getElementById('map-canvas');
-	homePos = new google.maps.LatLng(42.2813, -83.7483);
-
-	mapOptions = {
-    	center: homePos,
+    var mapOptions = {
         zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        panControl: false,
-        zoomControlOptions: {
-        	position: google.maps.ControlPosition.RIGHT_TOP
+        center: new google.maps.LatLng(42.2813, -83.7483),
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.BOTTOM_CENTER
         },
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        scaleControl: true,
+        streetViewControl: true,
         streetViewControlOptions: {
-        	position: google.maps.ControlPosition.RIGHT_TOP
+            position: google.maps.ControlPosition.RIGHT_CENTER
+
+
+        },
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
         }
+    }
 
-    };
-    
-    // Actual map variable (local for now - make this global)
-    map = new google.maps.Map(mapCanvas, mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    //apply the map-settings for panControl and zoomControl to the streetView:
+    map.get('streetView')
+        .setOptions({
+            panControlOptions: map.get('panControlOptions'),
+            zoomControlOptions: map.get('zoomControlOptions')
+        });
+
+
+    var center;
+
+    function calculateCenter() {
+        center = map.getCenter();
+    }
+    console.log(map);
+    google.maps.event.addDomListener(map, 'idle', function() {
+
+        calculateCenter();
+    });
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+
+    $('#closeButton').hide();
+
+    var streetView = map.getStreetView();
+
+    var closeButton = document.querySelector('#closeButton'),
+        controlPosition = google.maps.ControlPosition.RIGHT_TOP;
+
+    streetView.setOptions({
+        enableCloseButton: false
+    });
+
+    // Add to street view
+    streetView.controls[controlPosition].push(closeButton);
+
+    google.maps.event.addDomListener(streetView, 'visible_changed', function() {
+
+
+
+        if (streetView.getVisible()) {
+            $('#closeButton').show();
+            $('#closeButton').click(function() {
+                streetView.setVisible(false);
+            });
+        }
+    });
 }
-// I edited it
 
-// Add listener to call initialize function once page has loaded
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
